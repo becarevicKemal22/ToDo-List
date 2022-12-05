cae1d82ced1d6ff7db66435c57ab9c44e8cb86b1
becarevicKemal22 <116683338+becarevicKemal22@users.noreply.github.com>
2022-12-05 01:31:07 +0100

    Finished card name edit and list options menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,41 @@
 const listTemplate = document.getElementById("listTemplate");
 const cardTemplate = document.getElementById("cardTemplate");
 
-const cardModal = document.querySelector(".cardModal");
+let cardModal = document.querySelector(".cardModal");
+let optionsModal = document.querySelector(".optionsModal");
+
+function setModalCloseHandler(modal){
+    modal.addEventListener("click", () => {
+        modal.close();
+    });
+}
+
+function setUpOptionsModal(callList){
+    let clone = optionsModal.cloneNode(true);
+    document.querySelector('main').replaceChild(clone, optionsModal);
+    optionsModal = clone; 
+
+    setModalCloseHandler(optionsModal);
+
+    optionsModal.querySelector('.editName').addEventListener("click", () => {
+        optionsModal.close();
+        callList.focusInput();
+    });
+    optionsModal.querySelector('.deleteButton').addEventListener("click", () => {
+        optionsModal.close();
+        app.deleteList(callList);
+    });
+}
+
+function removeSpecificNode(el, index) {
+    var children = el.children;
+    if(children.length > 0) {
+        el.removeChild(children[index]);
+    }
+}
+
+setModalCloseHandler(cardModal);
+setModalCloseHandler(optionsModal);
 
 const addListButton = document.getElementById("addListButton");
 addListButton.addEventListener("click", () => {
@@ -24,6 +58,16 @@ class App {
             );
         newList.focusInput();
     }
+
+    deleteList(listToDelete){
+        const indeks = this.lists.findIndex(list => {
+            if(list == listToDelete){
+                return true;
+            }
+        })
+        removeSpecificNode(this.listContainer, indeks);
+        this.lists.splice(indeks, 1);        
+    }
 }
 
 class List {
@@ -40,6 +84,13 @@ class List {
             event.stopPropagation();
             this.addCard();
         });
+        this.optionsButton = this.HTMLElement.querySelector(".iconButton");
+        this.optionsModal = this.HTMLElement.querySelector(".listOptions");
+
+        //* Adds an event listener to the entire card container.
+        //* Skips if the event target was blank space.
+        //* If the rename button was clicked it changes the title to an input
+        //* And finally, it opens the modal of a card if it was clicked.
         this.cardContainer.addEventListener("click", (event) => {
             if (event.target.tagName.toLowerCase() === "ul") {
                 return;
@@ -48,18 +99,23 @@ class List {
                 event.target.tagName.toLowerCase() === "button" ||
                 event.target.tagName.toLowerCase() === "i"
             ) {
-                const crd = event.target.closest('.card');
-                const el = this.cards.find(card => card.id == crd.dataset.id);
+                const crd = event.target.closest(".card");
+                const el = this.cards.find((card) => card.id == crd.dataset.id);
                 el.changeTitleToInput();
-                
             } else {
                 cardModal.classList.add("visible");
             }
         });
+        //* Unfocuses the input field if enter is pressed.
         this.inputEl.addEventListener("keydown", (e) => {
             if (e.keyCode == 13) {
                 this.inputEl.blur();
             }
+        });
+
+        this.optionsButton.addEventListener("click", () => {
+            setUpOptionsModal(this);
+            optionsModal.showModal();
         });
     }
 
@@ -95,15 +151,16 @@ class Card {
             true
         );
         this.HTMLElement.dataset.id = id;
-        this.name = "";
         this.inputEl = this.HTMLElement.querySelector(".nameInput");
+        this.titleEl = null;
 
+        //* Unfocuses the element if the enter key is pressed
         this.inputEl.addEventListener("keydown", (e) => {
             if (e.keyCode == 13) {
                 this.inputEl.blur();
             }
         });
-
+        //* Converts the input element to a title element after it is unfocused.
         this.inputEl.addEventListener("blur", (e) => {
             this.changeInputToTitle();
         });
@@ -118,11 +175,11 @@ class Card {
         const newEl = document.createElement("h1");
         newEl.textContent = this.inputEl.value;
         newEl.classList.add("cardTitle");
-        this.title = newEl;
-        parent.replaceChild(newEl, this.inputEl);
+        this.titleEl = newEl;
+        parent.replaceChild(this.titleEl, this.inputEl);
     }
 
-    changeTitleToInput(){
+    changeTitleToInput() {
         const parent = this.HTMLElement.closest(".card");
         parent.replaceChild(this.inputEl, this.title);
         this.focusInput();
